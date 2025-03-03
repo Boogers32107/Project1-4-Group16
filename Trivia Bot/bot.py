917033e2cfa225d1073f19a7cf2c1b295af798e5
Jaden <boogers32107@gmail.com>
2025-03-03 20:41:06 +0000

    Replies added

--- Trivia Bot/bot.py
+++ Trivia Bot/bot.py
@@ -9,6 +9,9 @@ intents = discord.Intents.default()
 intents.message_content = True  
 client = discord.Client(intents=intents)
 
+current_question = None
+current_answer = None
+
 async def fetch_trivia(): #Pulls from URL
     url = 'https://opentdb.com/api.php?amount=1&category=15&type=boolean' #This is our Video Games API URL it will pull from a pool questions.
     async with aiohttp.ClientSession() as session:
@@ -25,6 +28,7 @@ async def on_ready():
 
 @client.event
 async def on_message(message):
+    global current_question, current_answer
     if message.author == client.user:
         return
 
@@ -32,9 +36,26 @@ async def on_message(message):
         questions = await fetch_trivia()
         if questions:
             for question in questions:
-                await message.channel.send(f"Category: {question['category']}\nQuestion: {question['question']}")
+                current_question = question['question']
+                current_answer = question['correct_answer']
+                await message.channel.send(f"Category: {question['category']}\nQuestion: {question['question']}\nReply with 'True' or 'False' otherwise it will break and give me an error.")
                 await asyncio.sleep(15) #Delay of 15 seconds so it doesn't get spammed.
         else:
             await message.channel.send("Please don't spam try again in a bit.")
+
+    elif current_question and message.content in ['True', 'False']:
+                if message.content == current_answer:
+                    await message.channel.send ("Correct")
+                else:
+                    await message.channel.send("Not Correct")
+                    current_question = None
+                    current_answer = None
+#@client.event
+#async def on_message(message):
+        #if message.author == client.user:
+            #return 
+        
+        #if message.content.startswith('!leaderboard'): #Will pull up Leaderboard
+
 
 client.run(TOKEN) #Pulls from the uptop token not doing a .env cause this is a private repo.
