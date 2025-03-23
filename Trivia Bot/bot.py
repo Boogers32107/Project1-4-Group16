07abe764103dbd2e3b3b65a7847e274a6f1002bd
Jaden <boogers32107@gmail.com>
2025-03-23 02:16:45 +0000

    Leaderboard Command and function

--- Trivia Bot/bot.py
+++ Trivia Bot/bot.py
@@ -1,22 +1,20 @@
 import discord
 import asyncio
 import aiohttp
-import json
-import html 
+import html
+from Leaderboard_1 import update_score, get_leaderboard
 
-
-TOKEN = 'MTMzNDI1OTIzODQ0MDI3NjA5Mg.GohmT3.bgCbmt6JuFo7FHPVayfE39ZYNscyGuVt5XnMdI' #Please don't abuse the token it is for my discord server with just me and a bot.
+TOKEN = 'YOUR_DISCORD_BOT_TOKEN'
 
 intents = discord.Intents.default()
-intents.message_content = True  
+intents.message_content = True
 client = discord.Client(intents=intents)
 
 current_question = None
 current_answer = None
 
-
-async def fetch_trivia(): #Pulls from URL
-    url = 'https://opentdb.com/api.php?amount=1&category=15&type=boolean' #This is our Video Games API URL it will pull from a pool questions.
+async def fetch_trivia():
+    url = 'https://opentdb.com/api.php?amount=1&category=15&type=boolean'
     async with aiohttp.ClientSession() as session:
         async with session.get(url) as response:
             if response.status == 200:
@@ -35,32 +33,32 @@ async def on_message(message):
     if message.author == client.user:
         return
 
-    if message.content.startswith('!trivia'): #This is the command it will watch in chat for.
+    if message.content.startswith('!trivia'):
         questions = await fetch_trivia()
         if questions:
             for question in questions:
                 current_question = question['question']
                 current_answer = question['correct_answer']
-                await message.channel.send(f"Category: {question['category']}\nQuestion: {html.unescape(question['question'])}\nReply with 'True' or 'False' otherwise it will break and give me an error.")
-                await asyncio.sleep(15) #Delay of 15 seconds so it doesn't get spammed.
+                await message.channel.send(f"Category: {question['category']}\nQuestion: {html.unescape(question['question'])}\nReply with 'True' or 'False'.")
+                await asyncio.sleep(15)
         else:
-            await message.channel.send("Please don't spam try again in a bit.")
+            await message.channel.send("Please try again later.")
 
     elif current_question and message.content in ['True', 'False']:
-                if message.content == current_answer:
-                    await message.channel.send ("Correct")
-                else:
-                    await message.channel.send("Not Correct")
-                    current_question = None
-                    current_answer = None
-   
-    
-#@client.event
-#async def on_message(message):
-        #if message.author == client.user:
-            #return 
-        
-        #if message.content.startswith('!leaderboard'): #Will pull up Leaderboard
+        if message.content == current_answer:
+            await message.channel.send("Correct")
+            update_score(str(message.author), 1)
+        else:
+            await message.channel.send("Incorrect")
+        current_question = None
+        current_answer = None
 
+    elif message.content.startswith('!leaderboard'):
+        lb = get_leaderboard()
+        if lb:
+            leaderboard_message = "\n".join([f"{entry['name']}: {entry['score']}" for entry in lb])
+            await message.channel.send(f"Leaderboard:\n{leaderboard_message}")
+        else:
+            await message.channel.send("Leaderboard is empty.")
 
-client.run(TOKEN) #Pulls from the uptop token not doing a .env cause this is a private repo.
+client.run(TOKEN)
